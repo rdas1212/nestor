88692c15015565b6053253fd7b0d4c5e3ad064a0
rdas <5216575+rdas1212@users.noreply.github.com>
2020-10-03 23:57:42 +0530

    API class impl 1

--- nestor/trello.py
+++ nestor/trello.py
@@ -1,17 +1,37 @@
 import logging
+import time
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional, List, Dict, Tuple
 
 LOG = logging.getLogger('nestor')
 
+CACHE_VALIDITY = 86400.0  # cache for a day
+
 
 @dataclass
-class Board(object):
+class TrelloList(object):
     name: str
     id: str
-    lists: Dict[str, str]
-    labels: Dict[str, str]
+
+
+@dataclass
+class TrelloLabel(object):
+    name: str
+    id: str
+    color: str
+
+
+@dataclass
+class TrelloBoard(object):
+    name: str
+    id: str
+    lists: List[TrelloList]
+    labels: List[TrelloLabel]
+
+
+class RequiredResourceMissingException(Exception):
+    pass
 
 
 class Trello(object):
@@ -23,7 +43,8 @@ class Trello(object):
         """
         self._api_key = api_key
         self._api_token = api_token
-        self._boards: Dict[str, Board] = dict()
+        self._boards: List[TrelloBoard] = list()
+        self._cache_time: float = 0.0
 
     def add_card(self, board_name: str, list_name: str, card_name: str, description: Optional[str] = None,
                  labels_names: Optional[List[str]] = None, due: Optional[datetime] = None,
@@ -39,18 +60,20 @@ class Trello(object):
         :param place_at_top: Optional flag to place the card on top of the list instead of the bottom
         """
         # TODO
-        # ensure the board is cached
-        self._cache_board(board_name)
+        # get the board
+        target_board = self._get_board(board_name)
+        if not target_board:
+            raise RequiredResourceMissingException(f'Board by name {board_name} does not exist')
         LOG.info('Board cached')
         # ensure the list exists in the board
-        self._ensure_list_exists(board_name, list_name)
+        target_list = self._get_list(target_board, list_name)
         LOG.info('List exists')
         # create the card with due-date, description & position
-        card_id = self._create_card(board_name, list_name, card_name, description, due, place_at_top)
-        LOG.info('Bare card created')
+        card_id = self._create_card(target_board, target_list, card_name, description, due, place_at_top)
+        LOG.info('Base card created')
         if labels_names:
             # get the valid labels
-            labels_to_add = self._get_valid_labels(board_name, labels_names)
+            labels_to_add = self._get_valid_labels(target_board, labels_names)
             # add the valid labels
             self._add_labels(card_id, labels_to_add)
             LOG.info('Labels added to card')
@@ -61,18 +84,47 @@ class Trello(object):
             LOG.info('Checklist created & added to card')
         LOG.info('Complete card created')
 
-    def _create_card(self, board_name: str, list_name: str, card_name: str,
-                     description: Optional[str] = None, due: Optional[datetime] = None,
-                     place_at_top: Optional[bool] = None) -> str:
+    def _get_board(self, board_name: str) -> Optional[TrelloBoard]:
+        cache_refreshed_already = False
+        # if the cache expired, refresh it
+        if not self.__cache_is_valid():
+            self._cache_all_boards()
+            cache_refreshed_already = True
+        # find in cache
+        cached_board = self.__get_cached_board(board_name)
+        if cached_board:
+            LOG.debug('Cached board was found')
+            return cached_board
+        # if not found in cache
+        if cache_refreshed_already:
+            # if cache was refreshed but board was still not found, then it's missing
+            LOG.error('Board was not found even after cache refresh')
+            return None
+        # if we hadn't refreshed the cached earlier, try refreshing again
+        self._cache_all_boards()
+        return self.__get_cached_board(board_name)
+
+    def _get_list(self, board: TrelloBoard, list_name: str) -> TrelloList:
+        # board is assumed to be fresh (i.e. from a valid cache)
+        cached_list = self.__get_cached_list(board, list_name)
+        if cached_list:
+            return cached_list
+        LOG.debug('List %s does not exist, will be created', list_name)
+        created_list = self._create_list(board, list_name)
+        LOG.info('List was created: %s', created_list)
+        return created_list
+
+    def _create_card(self, target_board: TrelloBoard, target_list: TrelloList, card_name: str,
+                     description: Optional[str], due: Optional[datetime], place_at_top: Optional[bool]) -> str:
         pass
 
-    def _cache_board(self, board_name: str) -> None:
+    def _create_list(self, target_board: TrelloBoard, list_name: str) -> TrelloList:
         pass
 
-    def _ensure_list_exists(self, board_name: str, list_name: str) -> None:
+    def _cache_all_boards(self) -> None:
         pass
 
-    def _get_valid_labels(self, board_name: str, label_names: List[str]) -> List[str]:
+    def _get_valid_labels(self, target_board: TrelloBoard, label_names: List[str]) -> List[str]:
         pass
 
     def _add_labels(self, card_id: str, label_ids: List[str]) -> None:
@@ -86,3 +138,20 @@ class Trello(object):
             'key': self._api_key,
             'token': self._api_token
         }
+
+    def __get_cached_board(self, board_name) -> Optional[TrelloBoard]:
+        searched_boards = [b for b in self._boards if b.name == board_name]
+        if not searched_boards:
+            return None
+        LOG.debug('Found %s boards by name %s', len(searched_boards), board_name)
+        return searched_boards[0]
+
+    def __get_cached_list(self, board: TrelloBoard, list_name) -> Optional[TrelloList]:  # noqa
+        searched_lists = [l for l in board.lists if l.name == list_name]
+        if not searched_lists:
+            return None
+        LOG.debug('Found %s lists by name %s in board %s', len(searched_lists), list_name, board.name)
+        return searched_lists[0]
+
+    def __cache_is_valid(self):
+        return self._boards and time.time() - self._cache_time <= CACHE_VALIDITY
